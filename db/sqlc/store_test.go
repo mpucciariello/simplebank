999be2e268452106dde3cd715e2d670dbdc6ca3f
Micaela Pucciariello <pucciariellomicaela@gmail.com>
2023-06-01 14:21:55 -0300

    added update balance test (tdd) to handle concurrent trx

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -13,11 +13,14 @@ func TestTxStore(t *testing.T) {
 	account2 := createRandomAccount(t)
 	amount := int64(1000)
 
+	n := 5
+	exists := make(map[int]bool)
+
 	errs := make(chan error) // channels allow goroutines to communicate. connects concurrent goroutines.
 	results := make(chan TransferTxResult)
 
 	// implement go routines to avoid concurrency issues
-	for i := 0; i < 5; i++ {
+	for i := 0; i < n; i++ {
 		go func() {
 			result, err := store.TransferTx(context.Background(), TransferTxParams{
 				FromAccountID: account1.ID,
@@ -31,7 +34,8 @@ func TestTxStore(t *testing.T) {
 	}
 
 	//check results
-	for i := 0; i < 5; i++ {
+	// n defines the number of executions
+	for i := 0; i < n; i++ {
 
 		err := <-errs
 		require.NoError(t, err)
@@ -76,5 +80,44 @@ func TestTxStore(t *testing.T) {
 		require.NotEmpty(t, toEntry)
 		require.NoError(t, err)
 
+		// check accounts - from account
+		fromAccount := result.FromAccountID
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
+		require.Equal(t, account1.Owner, fromAccount.Owner)
+
+		// check accounts - to account
+		toAccount := result.ToAccountID
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+		require.Equal(t, account2.Owner, toAccount.Owner)
+
+		// check accounts balance
+		diffAccount := account2.Balance - account1.Balance
+		diffResult := toAccount.Balance - fromAccount.Balance
+
+		require.Equal(t, diffAccount, diffResult)
+		require.True(t, diffResult > 0)
+		require.True(t, diffResult%amount == 0)
+
+		// n is the number of times the transfer has been done
+		z := int(diffResult / amount)
+		require.True(t, z > 0 && z <= n)
+
+		// k must be unique if concurrency is working properly
+		require.NotContains(t, exists, z)
+		exists[z] = true
+
+		// check updated accounts
+		updatedFromAccount, err := store.GetAccount(context.Background(), fromAccount.ID)
+		require.NotEmpty(t, updatedFromAccount)
+		require.NoError(t, err)
+
+		updatedToAccount, err := store.GetAccount(context.Background(), toAccount.ID)
+		require.NotEmpty(t, updatedToAccount)
+		require.NoError(t, err)
+
+		require.Equal(t, account1.Balance-int64(z)*amount, updatedFromAccount.Balance)
+		require.Equal(t, account2.Balance+int64(z)*amount, updatedToAccount.Balance)
 	}
 }
