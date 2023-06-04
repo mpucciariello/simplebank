dc2af829d216c989c0d2a25755291de6b4b4f8a3
Micaela Pucciariello <48954967+micaelapucciariello@users.noreply.github.com>
2023-06-04 11:28:06 -0300

    configure env (#6)
    * configured env using viper
    
    * configured env vars

--- api/server/account.go
+++ api/server/account.go
@@ -21,6 +21,10 @@ type (
 		PageID   int32 `form:"page_id" binding:"required,min=1"`
 		PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
 	}
+
+	deleteAccountReq struct {
+		ID int64 `uri:"id" binding:"required,min=1"`
+	}
 )
 
 func (s *Server) createAccount(ctx *gin.Context) {
@@ -79,5 +83,19 @@ func (s *Server) getAccountsList(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, errResponse(err))
 	} else {
 		ctx.JSON(http.StatusOK, account)
+	}
+}
+
+func (s *Server) deleteAccount(ctx *gin.Context) {
+	var req deleteAccountReq
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errResponse(err))
+	}
+
+	err := s.store.DeleteAccount(ctx, req.ID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errResponse(err))
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{"account": req.ID})
 	}
 }
--- main.go
+++ main.go
@@ -5,19 +5,19 @@ import (
 	"fmt"
 	api "github.com/micaelapucciariello/simplebank/api/server"
 	db "github.com/micaelapucciariello/simplebank/db/sqlc"
+	"github.com/micaelapucciariello/simplebank/utils"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	driverName    = "postgres"
-	sourceName    = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(driverName, sourceName)
+	cfg, err := utils.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot get config: ", err)
+	}
+
+	conn, err := sql.Open(cfg.DriverName, cfg.SourceName)
 	if err != nil {
 		log.Fatal(fmt.Sprintf("cannot connect to db: %s", err))
 	}
@@ -25,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.New(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(cfg.ServerAddress)
 	if err != nil {
 		log.Fatal(fmt.Sprintf("cannot start server: %s", err))
 	}
