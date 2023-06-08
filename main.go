3de19f743faf9948e496b5f4857df2af993dd578
Micaela Pucciariello <48954967+micaelapucciariello@users.noreply.github.com>
2023-06-08 10:16:20 -0300

    mock db (#9)
    * refactored code: added interfaces, generated mocks
    
    * added test matrix
    
    * added get account test cases

--- main.go
+++ main.go
@@ -5,19 +5,22 @@ import (
 	"fmt"
 	"github.com/micaelapucciariello/simplebank/api"
 	db "github.com/micaelapucciariello/simplebank/db/sqlc"
-	"github.com/micaelapucciariello/simplebank/utils"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	cfg, err := utils.LoadConfig(".")
-	if err != nil {
-		log.Fatal("cannot get config: ", err)
-	}
+	//cfg, err := utils.LoadConfig(".")
+	//if err != nil {
+	//	log.Fatal("cannot get config: ", err)
+	//}
 
-	conn, err := sql.Open(cfg.DriverName, cfg.SourceName)
+	driverName := "postgres"
+	sourceName := "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress := "0.0.0.0:8080"
+
+	conn, err := sql.Open(driverName, sourceName)
 	if err != nil {
 		log.Fatal(fmt.Sprintf("cannot connect to db: %s", err))
 	}
@@ -25,7 +28,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(cfg.ServerAddress)
+	err = server.Start(serverAddress)
 	if err != nil {
 		log.Fatal(fmt.Sprintf("cannot start server: %s", err))
 	}
