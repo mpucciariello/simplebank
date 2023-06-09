e47f2b24c52bbc674f1b90ee2406ba75dd72a590
Micaela Pucciariello <48954967+micaelapucciariello@users.noreply.github.com>
2023-06-09 16:11:39 -0300

    added users schema/model/queries (#11)

---
+++ db/query/users.sql
@@ -0,0 +1,32 @@
+-- name: CreateUser :one
+INSERT INTO users (username,
+                   hashed_password,
+                   full_name)
+VALUES ($1, $2, $3) RETURNING *;
+
+-- name: GetUser :one
+SELECT *
+FROM users
+WHERE username = $1 LIMIT 1;
+
+-- name: GetUserForUpdate :one
+SELECT *
+FROM users
+WHERE username = $1 LIMIT 1 FOR NO KEY
+UPDATE;
+
+-- name: ListUsers :many
+SELECT *
+FROM users
+ORDER BY username LIMIT $1
+OFFSET $2;
+
+-- name: UpdateUser :one
+UPDATE users
+SET hashed_password = $2, password_changed_at = $3
+WHERE username = $1 RETURNING *;
+
+-- name: DeleteUser :exec
+DELETE
+FROM users
+WHERE username = $1;
